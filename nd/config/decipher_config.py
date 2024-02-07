dfc0f4a97e87992e6c9925edf3ec607b5d8c6978
Hiroaki Sasaki <hiroaki.sasakiki@gmail.com>
2024-02-07 10:10:44 +0900

    add KorWuu class

--- nd/config/decipher_config.py
+++ nd/config/decipher_config.py
@@ -9,7 +9,8 @@ register = registry.register
 class UgaHebSmallNoSpe:
     lost_lang: str = 'uga-no_spe'
     known_lang: str = 'heb-no_spe'
-    cog_path: str = 'data/uga-heb.small.no_spe.cog'
+    #cog_path: str = '/home/s2230007/venv/python3.7/root/NeuroDecipher/data/uga-heb.small.no_spe.cog'   # hs 20221122 kagayaki
+    cog_path: str = 'data/uga-heb.small.no_spe.cog'                                                     # hs 20240109 colab
     num_cognates: int = 221
     num_epochs_per_M_step: int = 150
     eval_interval: int = 10
@@ -20,3 +21,22 @@ class UgaHebSmallNoSpe:
     capacity: int = 3
     dropout: float = 0.3
     warm_up_steps: int = 5
+    gpu: int = 0                                                                                        # hs 20240109
+
+@register
+class KorWuu:                                                                                           # hs 20240129
+    lost_lang: str = 'kor'
+    known_lang: str = 'wuu'
+    #cog_path: str = '/home/s2230007/venv/python3.7/root/NeuroDecipher/data/cognate.txt'                # hs 20240129 kagayaki
+    cog_path: str = 'data/cognate.txt'                                                                  # hs 20240129 colab
+    num_cognates: int = 220
+    num_epochs_per_M_step: int = 150
+    eval_interval: int = 10
+    check_interval: int = 10
+    num_rounds: int = 10
+    batch_size: int = 500
+    n_similar: int = 5
+    capacity: int = 3
+    dropout: float = 0.3
+    warm_up_steps: int = 5
+    gpu: int = 0            
